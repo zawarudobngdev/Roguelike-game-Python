68b9533197a6e397d0762ba60b3786c0221f5178
zawarudobngdev <murilo.m@hotmail.com>
2023-08-06 13:17:47 -0300

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tcod
 
 import copy
 
+import color
 from engine import Engine
 import entity_factories
 from procgen import generate_dungeon
@@ -12,7 +13,7 @@ def main() -> None:
     screen_height = 50
 
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 6
@@ -37,14 +38,20 @@ def main() -> None:
     )
     engine.update_fov()
 
+    engine.message_log.add_message(
+        "Hello, adventurer, and welcome to the dungeon.", color.welcome_text
+    )
+
     with tcod.context.new_terminal(screen_width, screen_height, tileset=tileset,
                                    title="Roguelike", vsync=True) as context:
         root_console = tcod.console.Console(screen_width, screen_height, order="F")
 
         while True:
-            engine.render(console=root_console, context=context)
+            root_console.clear()
+            engine.event_handler.on_render(console=root_console)
+            context.present(root_console)
 
-            engine.event_handler.handle_events()
+            engine.event_handler.handle_events(context)
 
 
 if __name__ == "__main__":
